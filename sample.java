3c8f5b092488d0f5fbcb0988072f7086cc618f5f
ajay <ajayyennawar5674@gmail.com>
2023-07-30 22:44:21 +0530

    third commit

--- sample.java
+++ sample.java
@@ -3,6 +3,9 @@
  class Sample{
 	 public static void main(String[] args){
 		 System.out.println("Sample program for print output");
+		 //////
+	// new change
+	System.out.println("new changes made in sample file");
 	 }
  }
 
